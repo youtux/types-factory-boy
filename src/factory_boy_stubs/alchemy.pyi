b549251d22e191b258ffed5f9e31cab8e3f31b26
Alessio Bogon <youtux@gmail.com>
2022-05-22 22:48:45 +0200

    Fix alchemy.pyi stub

--- src/factory_boy_stubs/alchemy.pyi
+++ src/factory_boy_stubs/alchemy.pyi
@@ -1,10 +1,10 @@
-from _typeshed import Incomplete
+from typing import List, Optional
 
-from . import base, errors
+from . import base
 
 SESSION_PERSISTENCE_COMMIT: str
 SESSION_PERSISTENCE_FLUSH: str
-VALID_SESSION_PERSISTENCE_TYPES: Incomplete
+VALID_SESSION_PERSISTENCE_TYPES: List[Optional[str]]
 
 class SQLAlchemyOptions(base.FactoryOptions): ...
 
