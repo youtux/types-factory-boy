a8fac9b2b6029e715361ff2dda4b9279f6afdfae
Alessio Bogon <youtux@gmail.com>
2022-05-22 22:46:39 +0200

    Fix utils.pyi stub

--- src/factory_boy_stubs/utils.pyi
+++ src/factory_boy_stubs/utils.pyi
@@ -1,23 +1,42 @@
-from _typeshed import Incomplete
+import collections
+from types import ModuleType
+from typing import (
+    Any,
+    Callable,
+    Generic,
+    Iterable,
+    Iterator,
+    List,
+    Mapping,
+    Optional,
+    Sequence,
+    TypeVar,
+)
 
-def import_object(module_name, attribute_name): ...
+T = TypeVar("T")
+
+def import_object(module_name: str, attribute_name: str) -> ModuleType: ...
 
 class log_pprint:
-    args: Incomplete
-    kwargs: Incomplete
-    def __init__(self, args=..., kwargs: Incomplete | None = ...) -> None: ...
+    args: Sequence[Any]
+    kwargs: Mapping[str, Any]
+    def __init__(
+        self, args: Sequence[Any] = ..., kwargs: Optional[Mapping[str, Any]] = ...
+    ) -> None: ...
 
-class ResetableIterator:
-    iterator: Incomplete
-    past_elements: Incomplete
-    next_elements: Incomplete
-    def __init__(self, iterator, **kwargs) -> None: ...
-    def __iter__(self): ...
+class ResetableIterator(Generic[T]):
+    iterator: Iterable[T]
+    past_elements: collections.deque[T]
+    next_elements: collections.deque[T]
+    def __init__(self, iterator: Iterable[T], **kwargs: Any) -> None: ...
+    def __iter__(self) -> Iterator[T]: ...
     def reset(self) -> None: ...
 
 class OrderedBase:
     CREATION_COUNTER_FIELD: str
-    def __init__(self, **kwargs) -> None: ...
+    def __init__(self, **kwargs: Any) -> None: ...
     def touch_creation_counter(self) -> None: ...
 
-def sort_ordered_objects(items, getter=...): ...
+def sort_ordered_objects(
+    items: Iterable[T], getter: Optional[Callable[[T], OrderedBase]] = ...
+) -> List[T]: ...
