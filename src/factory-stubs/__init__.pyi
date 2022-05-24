f3c560ccc240da77a65797882c416e4177e9ac6b
Alessio Bogon <youtux@gmail.com>
2022-05-25 01:04:23 +0200

    Temporarely export all the symbols from __init__.

--- src/factory-stubs/__init__.pyi
+++ src/factory-stubs/__init__.pyi
@@ -1,51 +1,51 @@
-from . import alchemy, django, mogo, mongoengine
-from .base import (
-    BaseDictFactory,
-    BaseListFactory,
-    DictFactory,
-    Factory,
-    ListFactory,
-    StubFactory,
-    use_strategy,
-)
-from .declarations import (
-    ContainerAttribute,
-    Dict,
-    Iterator,
-    LazyAttribute,
-    LazyAttributeSequence,
-    LazyFunction,
-    List,
-    Maybe,
-    PostGeneration,
-    PostGenerationMethodCall,
-    RelatedFactory,
-    RelatedFactoryList,
-    SelfAttribute,
-    Sequence,
-    SubFactory,
-    Trait,
-)
-from .enums import BUILD_STRATEGY, CREATE_STRATEGY, STUB_STRATEGY
-from .errors import FactoryError
-from .faker import Faker
-from .helpers import (
-    build,
-    build_batch,
-    container_attribute,
-    create,
-    create_batch,
-    debug,
-    generate,
-    generate_batch,
-    iterator,
-    lazy_attribute,
-    lazy_attribute_sequence,
-    make_factory,
-    post_generation,
-    sequence,
-    simple_generate,
-    simple_generate_batch,
-    stub,
-    stub_batch,
-)
+# TODO: Figure out which symbols should be actually exported here
+from . import alchemy as alchemy
+from . import django as django
+from . import mogo as mogo
+from . import mongoengine as mongoengine
+from .base import BaseDictFactory as BaseDictFactory
+from .base import BaseListFactory as BaseListFactory
+from .base import DictFactory as DictFactory
+from .base import Factory as Factory
+from .base import ListFactory as ListFactory
+from .base import StubFactory as StubFactory
+from .base import use_strategy as use_strategy
+from .declarations import ContainerAttribute as ContainerAttribute
+from .declarations import Dict as Dict
+from .declarations import Iterator as Iterator
+from .declarations import LazyAttribute as LazyAttribute
+from .declarations import LazyAttributeSequence as LazyAttributeSequence
+from .declarations import LazyFunction as LazyFunction
+from .declarations import List as List
+from .declarations import Maybe as Maybe
+from .declarations import PostGeneration as PostGeneration
+from .declarations import PostGenerationMethodCall as PostGenerationMethodCall
+from .declarations import RelatedFactory as RelatedFactory
+from .declarations import RelatedFactoryList as RelatedFactoryList
+from .declarations import SelfAttribute as SelfAttribute
+from .declarations import Sequence as Sequence
+from .declarations import SubFactory as SubFactory
+from .declarations import Trait as Trait
+from .enums import BUILD_STRATEGY as BUILD_STRATEGY
+from .enums import CREATE_STRATEGY as CREATE_STRATEGY
+from .enums import STUB_STRATEGY as STUB_STRATEGY
+from .errors import FactoryError as FactoryError
+from .faker import Faker as Faker
+from .helpers import build as build
+from .helpers import build_batch as build_batch
+from .helpers import container_attribute as container_attribute
+from .helpers import create as create
+from .helpers import create_batch as create_batch
+from .helpers import debug as debug
+from .helpers import generate as generate
+from .helpers import generate_batch as generate_batch
+from .helpers import iterator as iterator
+from .helpers import lazy_attribute as lazy_attribute
+from .helpers import lazy_attribute_sequence as lazy_attribute_sequence
+from .helpers import make_factory as make_factory
+from .helpers import post_generation as post_generation
+from .helpers import sequence as sequence
+from .helpers import simple_generate as simple_generate
+from .helpers import simple_generate_batch as simple_generate_batch
+from .helpers import stub as stub
+from .helpers import stub_batch as stub_batch
