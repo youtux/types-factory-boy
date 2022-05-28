96e3b07c2b56c8db4d147f07494b146401949e92
Alessio Bogon <alessio.bogon@b2c2.com>
2022-05-28 22:05:19 +0200

    Fix vendor factories types

--- src/factory-stubs/alchemy.pyi
+++ src/factory-stubs/alchemy.pyi
@@ -1,13 +1,15 @@
-from typing import List, Optional
+from typing import List, Optional, TypeVar
 
 from . import base
+
+T = TypeVar("T")
 
 SESSION_PERSISTENCE_COMMIT: str
 SESSION_PERSISTENCE_FLUSH: str
 VALID_SESSION_PERSISTENCE_TYPES: List[Optional[str]]
 
-class SQLAlchemyOptions(base.FactoryOptions): ...
+class SQLAlchemyOptions(base.FactoryOptions[T]): ...
 
-class SQLAlchemyModelFactory(base.Factory):
+class SQLAlchemyModelFactory(base.Factory[T]):
     class Meta:
         abstract: bool
--- src/factory-stubs/mogo.pyi
+++ src/factory-stubs/mogo.pyi
@@ -1,5 +1,9 @@
+from typing import TypeVar
+
 from . import base
 
-class MogoFactory(base.Factory):
+T = TypeVar("T")
+
+class MogoFactory(base.Factory[T]):
     class Meta:
         abstract: bool
